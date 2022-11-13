49d9402753c821a0d3e61682906750eab901503c
bohe3212 <114132286+bohe3212@users.noreply.github.com>
2022-11-13 10:40:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,13 +8,13 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wxa6ac8b8fe0d54826',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '86c49c80ae359ad4ecae58c915baf431',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '浙江',
+  CITY: '东阳',
 
   USERS: [
     {
